837c2466773068563e0ae68ef889b45c4aa7c80d
Atsuki Demizu <demmys.atsuki@gmail.com>
2013-11-05 00:47:43 +0900

    implemented: event system

--- js/event.js
+++ js/event.js
@@ -13,9 +13,7 @@
  * HTMLの要素読み込み後に即時実行されるメソッド
  */
 $(function(){
-    $.getJSON("/event/eventlist.json", function(data){
-        kbc.event.initialize("/event/eventlist.json");
-    });
+    kbc.event.initialize(1, "/event/recentevent.json", "/event/pastevent.json");
 });
 
 
@@ -36,14 +34,98 @@ $(function(){
         lib[namespace] = ns;
     }
 
-    ns.initialize = function(json){
-        $.getJSON(json, function(list){
-            var i = 0;
-            list.forEach(function(e){
-                $(".kbc-main .kbc-container-inner").append($("<div>").attr("id", "kbc-event-" + i));
-                kbc.view.event($("#kbc-event-" + i), e);
-                i++;
-            });
+    var EventDescriptor = function($elem, filter, book){
+        this.$elem = $elem;
+        this.filter = filter;
+        this.book = book;
+        this.curPage = 0;
+        this.curLine = 0;
+    };
+    EventDescriptor.prototype = {
+        appendLine: function(num, defaultClasses){
+            if(this.curPage >= this.book.length){
+                return;
+            }
+            var dist = this.curLine + num;
+            $.getJSON(this.book[this.curPage], (function(lines){
+                while(this.curLine < dist){
+                    if(lines.length <= this.curLine){
+                        this.curPage++;
+                        num = dist - this.curLine;
+                        this.curLine = 0;
+                        this.appendLine(num, defaultClasses);
+                        return;
+                    }
+                    var line = lines[this.curLine];
+                    if(this.filter(line)){
+                        var classes = "event-headline";
+                        if(defaultClasses){
+                            classes += " " + defaultClasses;
+                        }
+                        kbc.view.event(this.$elem, line, classes);
+                    } else{
+                        dist++;
+                    }
+                    this.curLine++;
+                }
+            }).bind(this));
+        }
+    };
+
+    ns.initialize = function(newPageNo, jsons){
+        var newJsons = new Array();
+        var pastJsons = new Array();
+        for(var i = 1; i < arguments.length; i++){
+            if(i <= newPageNo){
+                newJsons.push(arguments[i]);
+            }
+            pastJsons.push(arguments[i]);
+        }
+
+        var newEvent = new EventDescriptor($("#new"), function(event){
+            return !isPast(event.date);
+        }, newJsons);
+        var pastEvent = new EventDescriptor($("#past"), function(event){
+            return isPast(event.date);
+        }, pastJsons);
+
+        newEvent.appendLine(3);
+        pastEvent.appendLine(3, "hide");
+
+        $("#new-btn").click(function(){
+            newEvent.appendLine(3);
         });
-    }
+        $("#past-btn").click(function(){
+            pastEvent.appendLine(3);
+        });
+
+        $("a[data-toggle=\"tab\"]").click(function(){
+            $(".kbc-nav-list>.event-headline").toggleClass("hide");
+            $(".more-btn").toggleClass("hide");
+        });
+    };
+
+    var isPast = function(theDate){
+        var today = new Date();
+        var year = today.getFullYear(),
+            month = today.getMonth() + 1,
+            date = today.getDate();
+
+        if(theDate.year < year){
+            return true;
+        } else if(theDate.year > year){
+            return false;
+        }
+        if(theDate.month < month){
+            return true;
+        } else if(theDate.month > month){
+            return false;
+        }
+        if(theDate.date < date){
+            return true;
+        } else if(theDate.month > date){
+            return false;
+        }
+        return false;
+    };
 }(this, "kbc", "event"));
